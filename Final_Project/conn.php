8b7bbd84a92da7925b5b5d4031e76ab6b3aa504a
Zvrok <110719882+Zvrok@users.noreply.github.com>
2022-08-06 21:26:09 +0700

    Delete Final_Project directory

--- Final_Project/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-//buat koneksi
-$conn = mysqli_connect("localhost", "root", "", "datafinalproject");
-//cek koneksi
-if ($conn->connect_error){
-    die("Koneksi Gagal:" . $conn->connect_error);
-}
-?>
